884eeb2ebfdc361170a48e52e0a354a663600ebd
SSKubyshkin <SSKubyshkin@gmail.com>
2022-01-23 19:02:50 +1200

    Update send-command.sh

--- send-command.sh
+++ send-command.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-set -e
-
-if [ "$1" = "" ] || [ "$2" = "" ]; then
-  echo "Usage: $0 <TV_IP> <IRCC_COMMAND>"
-  exit 1
-fi
+# A shell script to send a command to a TV. The TV will execute the command and immediately return.
+# Taken from: https://github.com/breunigs/bravia-auth-and-remote
 
 cmd="<?xml version=\"1.0\"?><s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><s:Body><u:X_SendIRCC xmlns:u=\"urn:schemas-sony-com:service:IRCC:1\"><IRCCCode>$2</IRCCCode></u:X_SendIRCC></s:Body></s:Envelope>"
 
-# X-Auth_PSK: H127CXS is a hard-coded value.
+# X-Auth_PSK: H127CXS is a Pre-Shared Key.
 code=$(curl -w "%{http_code}" --silent -XPOST http://$1/sony/IRCC -d "$cmd" -H 'Content-Type: text/xml; charset=UTF-8' -H 'SOAPACTION: "urn:schemas-sony-com:service:IRCC:1#X_SendIRCC"' -H "X-Auth-PSK: H127CXS" -o /dev/null )
 
 if [ "$code" = "200" ]; then
-  echo "✓"
+  echo "Success!"
 else
-  echo "Command failed (HTTP_CODE: $code, try running it in a console)"
+  echo "Command failed (HTTP_CODE: $code)"
   exit 1
 fi
