5ec71a968ff44cf5402396953ee0701908bb27d1
SSKubyshkin <SSKubyshkin@gmail.com>
2022-02-09 19:43:01 +1200

    Update bravia-remote.c

--- bravia-remote.c
+++ bravia-remote.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 
 char* concat(const char *s1, const char *s2) {
-  char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+  char *result = malloc(strlen(s1) + strlen(s2) + 1);
   strcpy(result, s1);
   strcat(result, s2);
   return result;
